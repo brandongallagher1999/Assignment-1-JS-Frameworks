eb203c629bf7b1fb30d1e628be58e9fe37db1964
Brandon Gallagher <brandonegallagher@gmail.com>
2020-11-02 14:14:21 -0500

    Added react stuff... etc.

---
+++ frontend/src/modules/NavBar.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, Fragment} from 'react';
+
+function NavBar()
+{
+
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        setName("Brandon");
+    }, []);
+
+    return (
+        <Fragment>
+            <nav class="navbar" style={{ fontSize: "20px", margin: "45px"}}>
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                        <span style={{fontFamily: "Brush Script MT"}}>
+                            {name}
+                        </span>
+                    </a>
+                </div>
+                <div class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item">
+                            Home
+                        </a>
+                        <a class="navbar-item">
+                            Education
+                        </a>
+                        <a class="navbar-item">
+                            Experience
+                        </a>
+                        <a class="navbar-item">
+                            Projects
+                        </a>
+                        <a class="navbar-item">
+                            Open Source
+                        </a>
+                        <a class="navbar-item">
+                            Contact Me
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </Fragment>
+    );
+}
+
+export default NavBar;
